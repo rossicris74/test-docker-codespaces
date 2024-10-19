d3bf0736a5f6efa92e47e838f678f4958cfcad2e
rossicris74 <118047048+rossicris74@users.noreply.github.com>
2024-10-19 14:30:37 +0000

    Added columns

--- MysqlScripts/scCorsi.sql
+++ MysqlScripts/scCorsi.sql
@@ -3,6 +3,7 @@ create table corsi (
   id          int  not null auto_increment,
   codice      int(8) not null,
   descrizione varchar(50) not null,
+  ultima_modifica datetime not null default NOW(),
   primary key(id)
   );
 alter table corsi add unique INDEX IDX_CORSI_001(codice);
@@ -10,3 +11,5 @@ insert into corsi (codice,descrizione) values (1,'Corso SqlBase');
 insert into corsi (codice,descrizione) values (2,'Corso SqlAvanzato');
 insert into corsi (codice,descrizione) values (3,'Corso Css');
 insert into corsi (codice,descrizione) values (4,'Corso Html');
+
+select * from corsi;
