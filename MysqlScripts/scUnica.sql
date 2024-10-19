592499d056ce7dd9b7b7e99d16a7efc5f9d37ccb
rossicris74 <118047048+rossicris74@users.noreply.github.com>
2024-10-19 14:05:05 +0000

    Unita Script Modified

--- MysqlScripts/scUnica.sql
+++ MysqlScripts/scUnica.sql
@@ -4,9 +4,10 @@ create table Unica (
     CodCorso                INT,
     NomeCorso               VARCHAR(50),
     DescrizioneCorso        VARCHAR(100),
-    DataCorso               Date,
+    DataInizioCorso         DateTime not null default NOW(),
+    DataFineCorso           DateTime not null default NOW(),
     CodiceCorsista          INT,
     CognomeCorsista         VARCHAR(50),
     NomeCorsista            VARCHAR(50),
-    DataNascitaCorsista     Date 
+    DataNascitaCorsista     Date
 );
